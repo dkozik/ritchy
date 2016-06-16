4b135f63fe6f7cd5f78744f02591c5ba02d1695b
Kozik Denis <dkozik@yandex.ru>
2016-06-16 23:46:46 +0300

    Добавил API на форму восстановления аккаунта Добавил кнопку Cancel на форму восстановления аккаунта
    https://bitbucket.org/emirit/ritchy/issues/8/api

--- assets/modules/recovery/recovery.js
+++ assets/modules/recovery/recovery.js
@@ -4,15 +4,53 @@
 
 (function() {
 
-    RitchyApp.controller('recovery', ['$scope', '$element', 'RitchyApi', 'RitchyDialog', '$location',
-        function($scope, $element, RitchyApi, RitchyDialog, $location) {
+    RitchyApp.controller('recovery', ['$scope', '$rootScope', 'RitchyApi', 'RitchyDialog', 'RitchyAnim', '$location',
+        function($scope, $rootScope, RitchyApi, RitchyDialog, RitchyAnim, $location) {
 
             // Animation
-            var tl = new TimelineMax();
             var target = document.querySelector("div.recovery-pane");
-            TweenLite.set(target, {scale: 0.5, autoAlpha:0});
-            tl.to(target, 0.3, {autoAlpha: 1});
-            tl.to(target, 0.5, {scale: 1, ease: Back.easeOut.config(0.6), autoRound: false}, 0);
-        }]);
+            RitchyAnim.easeIn(target);
+            
+            $scope.user = {
+                email: '',
+                canRecovery: false,
+                changed: function() {
+                    this.canRecovery = this.email>'';
+                },
+                doCancel: function() {
+                    RitchyAnim.easeOut(target, function() {
+                        $rootScope.$apply(function() {
+                            $location.path('/login');
+                        });
+                    });
+                },
+                doRecovery: function( ev ) {
+                    RitchyApi.request('recovery', null, {email: this.email},
+                    function onSuccess( response ) {
+                        if (response.data.code==1) {
+                            RitchyDialog.showAlert('Recovery result', 'Password reset instructions sent to email', function() {
+                                RitchyAnim.easeOut(target, function() {
+                                    $rootScope.$apply(function() {
+                                        $location.path('/login');
+                                    });
+                                });
+                            });
+                        } else if (response.data.error>'') {
+                            RitchyDialog.showAlert('Login error', response.data.error);
+                        }
+                    }, function onError(response) {
+                            if (angular.isObject(response)) {
+                                if (response.status<0) {
+                                    RitchyDialog.showAlert('Login error', 'Unknown api error, response status ' + response.status+', details in console log.');
+                                } else {
+                                    RitchyDialog.showAlert('Login error', 'Unknown api error, response status '+ response.status+', response text: '+response.responseText);
+                                }
+                            } else {
+                                RitchyDialog.showAlert('Login error', 'Unknown api error: '+response);
+                            }
+                    });
+                }
+            };
 
+        }]);
 })();
